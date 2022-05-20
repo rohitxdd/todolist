132e73ca48821bda8fc1cc52bd25505ae918cb99
Rohit <rohitsharma998850@gmail.com>
2022-05-20 17:11:35 +0530

    Connected app to mongodb and added node_modules directory to gitignore

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 // importing node modules
 const express = require("express");
+const mongoose = require("mongoose");
 
-var NewItems = ["Just do it"];
-let workList = [];
-// init express
+
+//mongoose connect
+mongoose.connect("mongodb://localhost:27017/toDoListDB")
+
+//creating schema
+const toDoSchema = {
+    name: {
+        type: String,
+        required: true
+    }
+}
+
+// mongoose model
+const Items = new mongoose.model("Item", toDoSchema);
+
+// inserting default Items
+const item1 = new Items({
+    name: "Welcome to to-do-list"
+})
+
+const item2 = new Items({
+    name: "Hit the + button to add new item"
+})
+
+const item3 = new Items({
+    name: "<-- Hit this to delete an item"
+})
+
+const defaultItem = [item1, item2, item3];
+
 const app = express();
-
 app.set('view engine' , 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,23 +42,49 @@ app.get("/", (req,res)=>{
     var options = { weekday: 'long', month: 'long', day: 'numeric' };
     var today = new Date;
     var Day = today.toLocaleDateString(undefined, options)
-    res.render("lists", {KindOfDay: Day, itemarr:NewItems})
+    Items.find({}, function(err, itemarr){
+        if(err){
+            console.log(err);
+        }else if(itemarr.length === 0){
+            Items.insertMany(defaultItem, function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("default items inserted");
+                }
+            })
+            res.redirect("/")
+        }else{
+            res.render("lists", {KindOfDay: Day, itemarr:itemarr})
+        }
+        
+    })
+    
 })
 
-
-app.get("/work", (req,res)=>{
-    res.render("lists", {KindOfDay: "Work List", itemarr : workList})
-})
 
 app.post("/", (req , res )=>{
-    if(req.body.list === "Work List"){
-        workList.push(req.body.NewItem)
-        res.redirect("/work")
+    if (req.body.NewItem != ""){
+        let newItemString = new Items({
+            name: req.body.NewItem
+        })
+        newItemString.save()
+        res.redirect("/")
     }else{
-        NewItem = req.body.NewItem;
-        NewItems.push(NewItem);
         res.redirect("/")
     }
+
+})
+
+
+app.post("/delete", function(req,res){
+    console.log(req.body.checkbox)
+    Items.deleteOne({_id:req.body.checkbox}, (err)=>{
+        if(err){
+            console.log(err)
+        }
+    })
+    res.redirect("/")
 })
 
 //init the server to 3000 port
